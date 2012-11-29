fede05afd393ed69428a9d90b63e961bcc9f7c72
Ilya Shpigor <petrsum@gmail.com>
2012-11-29 13:14:33 +0400

    Split sources to server daemon and client library

---
+++ source/server/zero_cached.cpp
@@ -0,0 +1,13 @@
+#include "reactor.h"
+
+using namespace zero_cache;
+
+int main()
+{
+    Reactor reactor;
+
+    reactor.Start();
+
+    while (true)
+        usleep(1000);
+}
--- source/types_zero_cache.h
+++
@@ -1,9 +0,0 @@
-#ifndef TYPES_ZERO_CACHE_H
-#define TYPES_ZERO_CACHE_H
-
-namespace zero_cache
-{
-
-}
-
-#endif
