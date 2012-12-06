ec310b53319d6a84d5dced81b1e1f2bc4c6cbaf7
Ilya Shpigor <petrsum@gmail.com>
2012-12-06 11:25:54 +0400

    Implement KeyList class methods

--- source/server/key_list.cpp
+++ source/server/key_list.cpp
@@ -1,15 +1,31 @@
 #include "key_list.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+
 using namespace std;
 using namespace zero_cache;
 
+KeyList::KeyList(string start_port)
+{
+    current_port_ = atoi(start_port.c_str());
+    current_port_++;
+}
+
 void KeyList::AddKey(string key)
 {
-    /* FIXME: Implement this method */
+    char port[10];
+    sprintf(port, "%d", current_port_);
+
+    ports_.insert(KeyPort::value_type(key, port));
+
+    /* FIXME: Increment current port on key count limit */
 }
 
 string KeyList::GetConnection(string key)
 {
-    /* FIXME: Implement this method */
-    return "";
+    if ( ports_.count(key) != 0 )
+        return ports_[key];
+    else
+        return "";
 }
--- source/server/key_list.h
+++ source/server/key_list.h
@@ -11,13 +11,19 @@ namespace zero_cache
 
 class KeyList
 {
+private:
+    typedef std::map<std::string, std::string> KeyPort;
+
 public:
-    KeyList() {};
+    KeyList(std::string start_port);
 
     void AddKey(std::string key);
     std::string GetConnection(std::string key);
 
 private:
+    int current_port_;
+    KeyPort ports_;
+
     DISALLOW_COPY_AND_ASSIGN(KeyList)
 };
 
