d4c9e6e29f330f2d2f698a1f0c9f60dc36c66692
Ilya Shpigor <petrsum@gmail.com>
2012-12-14 17:19:47 +0400

    Add test for the TCP socket type

---
+++ test/tcp_test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <czmq.h>
+
+#include "registrar_client.h"
+#include "speed_test.h"
+
+using namespace std;
+using namespace zero_cache;
+
+static const string kKey = "key2";
+static char kData[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+void InitData(RegistrarClient& client)
+{
+    client.WriteData(kKey, &kData, sizeof(kData));
+}
+
+void CheckData(RegistrarClient& client)
+{
+    char* result;
+    result = static_cast<char*>(client.ReadData(kKey));
+    assert( ! memcmp(result, kData, sizeof(kData)) );
+    free(result);
+}
+
+int main()
+{
+    RegistrarClient client("tcp_test.log", "tcp://localhost:5570", kDealer);
+
+    cout << "Start test..." << endl;
+
+    InitData(client);
+
+    CheckData(client);
+}
