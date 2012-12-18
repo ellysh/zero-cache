3ab53d2254e3f211b9240533110d1ff6402b9c2c
Ilya Shpigor <petrsum@gmail.com>
2012-12-18 14:41:37 +0400

    Add the empty ClientList class

---
+++ source/client/client_list.cpp
@@ -0,0 +1,4 @@
+#include "client_list.h"
+
+using namespace std;
+using namespace zero_cache;
---
+++ source/client/client_list.h
@@ -0,0 +1,15 @@
+#ifndef CLIENT_LIST_H
+#define CLIENT_LIST_H
+
+namespace zero_cache
+{
+
+class ClientList
+{
+public:
+    ClientList();
+};
+
+}
+
+#endif
