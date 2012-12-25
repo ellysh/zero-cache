23878b5d9bce9543815c0a18512e020969f7ea5b
Ilya Shpigor <petrsum@gmail.com>
2012-12-25 10:54:23 +0400

    PortCounter: Change the port increment value to 1 Remove socket files in the clean.sh script

--- bin/clean.sh
+++ bin/clean.sh
@@ -6,3 +6,5 @@ for (( i=1; i<=16; i++ ))
 do
     rm $i
 done
+
+rm -f /var/run/zero-cache/*
--- source/server/port_counter.cpp
+++ source/server/port_counter.cpp
@@ -7,7 +7,7 @@ using namespace zero_cache;
 
 PortCounter::PortCounter(int port, int limit) : limit_(limit), counter_(1)
 {
-    port_ = port + 2;
+    port_ = port + 1;
 }
 
 void PortCounter::Increment()
