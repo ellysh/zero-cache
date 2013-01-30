04e917c30a1a53593eebeb34e8c3eca14b1a83d3
Ilya Shpigor <petrsum@gmail.com>
2013-01-30 10:59:51 +0400

    Implement interface of the Request class

--- source/common/request.h
+++ source/common/request.h
@@ -1,13 +1,36 @@
 #ifndef REQUEST_H
 #define REQUEST_H
 
+#include <string>
+
+#include <zmq.h>
+
+#include "types_zcache.h"
+
 namespace zero_cache
 {
+
+class Socket;
 
 class Request
 {
 public:
-    Request() {};
+    Request();
+    ~Request();
+
+    void SetPreamble(Command command, port_t id, std::string& host);
+    void SetKey(std::string& key);
+    void SetData(void* data, size_t size);
+
+    void Send(Socket& socket);
+    void Receive(Socket& socket);
+
+private:
+    zmq_msg_t command_msg_;
+    zmq_msg_t id_msg_;
+    zmq_msg_t host_msg_;
+    zmq_msg_t key_msg_;
+    zmq_msg_t data_msg_;
 };
 
 }
