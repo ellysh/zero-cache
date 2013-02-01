74617d181246aa70b0239cc3eff7dee94284e1f3
Ilya Shpigor <shpigor@etersoft.ru>
2013-02-01 13:52:40 +0400

    Add test for empty value reading

--- test/get_test.cpp
+++ test/get_test.cpp
@@ -15,6 +15,8 @@ static const string kData1 = "test data 1";
 static const string kKey2 = "key2";
 static char kData2[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+static const string kKey3 = "key3";
+
 void InitData(RegistrarClient& client)
 {
     client.WriteData(kKey1, const_cast<char*>(kData1.c_str()), kData1.size());
@@ -30,6 +32,10 @@ void CheckData(RegistrarClient& client)
 
     result = static_cast<char*>(client.ReadData(kKey2));
     assert( ! memcmp(result, kData2, sizeof(kData2)) );
+    free(result);
+
+    result = static_cast<char*>(client.ReadData(kKey3));
+    assert( strlen(result) == 0 );
     free(result);
 }
 
