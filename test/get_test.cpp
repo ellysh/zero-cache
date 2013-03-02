f284168b6992d88e62d10a36b950d2c828d62b11
Ilya Shpigor <petrsum@gmail.com>
2013-03-02 12:45:36 +0400

    Fix get_test according the new Client class interface

--- test/get_test.cpp
+++ test/get_test.cpp
@@ -9,46 +9,18 @@
 using namespace std;
 using namespace zero_cache;
 
-static const string kKey1 = "key1";
-static const string kData1 = "test data 1";
-
-static const string kKey2 = "key2";
-static char kData2[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-static const string kKey3 = "key3";
+static const size_t kOffset1 = 0;
+static const long kData1 = 1024;
 
 void InitData(Client& client)
 {
-    Package package;
-    package.offset = 0;
-    package.pointer = malloc(kData1.size());
-    memcpy(package.pointer, kData1.c_str(), kData1.size());
-    package.size = kData1.size();
-
-    client.WriteData(kKey1, package);
-    free(package.pointer);
-
-    package.offset = sizeof(size_t) + kData1.size();
-    package.pointer = malloc(sizeof(kData2));
-    memcpy(package.pointer, &kData2, sizeof(kData2));
-    package.size = sizeof(kData2);
-
-    client.WriteData(kKey2, package);
-    free(package.pointer);
+    client.WriteLong(kOffset1, kData1);
 }
 
 void CheckData(Client& client)
 {
-    Package result = client.ReadData(kKey1);
-    assert( ! memcmp(result.pointer, kData1.c_str(), kData1.size()) );
-    free(result.pointer);
-
-    result = client.ReadData(kKey2);
-    assert( ! memcmp(result.pointer, kData2, sizeof(kData2)) );
-    free(result.pointer);
-
-    result = client.ReadData(kKey3);
-    assert( result.pointer == NULL );
+    long result = client.ReadLong(kOffset1);
+    assert( result == kData1 );
 }
 
 int main()
