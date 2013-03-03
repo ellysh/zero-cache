ae2b8d2f6caa4628f722cd7fab056d3898fc376e
Ilya Shpigor <petrsum@gmail.com>
2013-03-03 14:08:01 +0400

    Implement test for measure delays of the reading and writing operations

---
+++ test/read_test.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "client.h"
+#include "thread.h"
+
+using namespace std;
+using namespace zero_cache;
+
+void* WriteReadLoop(void* args)
+{
+    const char* log_file = static_cast<char*>(args);
+    Client client(log_file);
+
+    char* endptr;
+    size_t offset = strtoul(log_file, &endptr, 10);
+    long data = offset;
+    long result;
+
+    while (true)
+    {
+        client.WriteLong(offset, data);
+        usleep(1000);
+
+        result = client.ReadLong(offset);
+        assert( result == data );
+        usleep(1000);
+    }
+}
+
+void StartWriteThread(const char* args)
+{
+    CreateThread(WriteReadLoop, const_cast<char*>(args));
+}
+
+void WaitInput()
+{
+    char key;
+    cin >> key;
+}
+
+int main()
+{
+    cout << "Start test..." << endl;
+
+    StartWriteThread("1");
+    StartWriteThread("2");
+    StartWriteThread("3");
+    StartWriteThread("4");
+    StartWriteThread("5");
+    StartWriteThread("6");
+    StartWriteThread("7");
+    StartWriteThread("8");
+    StartWriteThread("9");
+    StartWriteThread("10");
+    StartWriteThread("11");
+    StartWriteThread("12");
+    StartWriteThread("13");
+    StartWriteThread("14");
+    StartWriteThread("15");
+    StartWriteThread("16");
+
+    WaitInput();
+}
