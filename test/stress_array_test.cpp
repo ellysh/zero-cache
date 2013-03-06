d872c28a0c536cbb049945b7319180ae0fe4754b
Ilya Shpigor <petrsum@gmail.com>
2013-03-06 16:44:44 +0400

    Implement the stress test for the array data operations

---
+++ test/stress_array_test.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "untyped_client.h"
+#include "thread.h"
+
+using namespace std;
+using namespace zero_cache;
+
+void* WriteReadLoop(void* args)
+{
+    const char* log_file = static_cast<char*>(args);
+    UntypedClient client(log_file);
+
+    char* endptr;
+    size_t index = strtoul(log_file, &endptr, 10);
+    char result[10];
+
+    while (true)
+    {
+        client.WriteArray(index, log_file, strlen(log_file));
+        usleep(1000);
+
+        client.ReadArray(index, result, strlen(log_file));
+        assert( ! memcmp(result, log_file, strlen(log_file)) );
+        usleep(1000);
+    }
+}
+
+void StartWriteThread(const char* args)
+{
+    CreateThread(WriteReadLoop, const_cast<char*>(args));
+}
+
+void WaitInput()
+{
+    char key;
+    cin >> key;
+}
+
+int main()
+{
+    cout << "Start test..." << endl;
+
+    StartWriteThread("1");
+    StartWriteThread("2");
+    StartWriteThread("3");
+    StartWriteThread("4");
+    StartWriteThread("5");
+    StartWriteThread("6");
+    StartWriteThread("7");
+    StartWriteThread("8");
+    StartWriteThread("9");
+    StartWriteThread("10");
+    StartWriteThread("11");
+    StartWriteThread("12");
+    StartWriteThread("13");
+    StartWriteThread("14");
+    StartWriteThread("15");
+    StartWriteThread("16");
+
+    WaitInput();
+}
